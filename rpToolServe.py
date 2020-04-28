923a4aedb739a3fbbb09201b5ee646388aca2683
mdulac <melchior.dulac@inra.fr>
2020-04-28 17:25:07 +0200

    totest

--- rpToolServe.py
+++ rpToolServe.py
@@ -4,8 +4,10 @@ import tempfile
 import tarfile
 import glob
 import logging
+import os
 
 import rpTool
+import rpSBML
 
 
 def runRPSBMLtoSBOL_hdd(inputTar, 
@@ -13,13 +15,38 @@ def runRPSBMLtoSBOL_hdd(inputTar,
                         rbs=True,
                         max_prot_per_react=3, 
                         tirs=None, 
+                        topX=5,
                         pathway_id='rp_pathway'):
+    logging.info(rbs)
     with tempfile.TemporaryDirectory() as tmpInputFolder:
         tar = tarfile.open(inputTar, mode='r')
         tar.extractall(path=tmpInputFolder)
         tar.close()
+        #return only the topX global score pathways
         if len(glob.glob(tmpInputFolder+'/*'))==0:
             logging.error('Input file is empty')
             return False
+        file_names_score = {}
+        for sbml_path in glob.glob(tmpInputFolder+'/*'):
+            file_name = sbml_path.split('/')[-1].replace('.sbml', '').replace('.xml', '').replace('.rpsbml', '')
+            rpsbml = rpSBML.rpSBML(file_name)
+            rpsbml.readSBML(sbml_path)
+            groups = rpsbml.model.getPlugin('groups')
+            rp_pathway = groups.getGroup(pathway_id)
+            if not rp_pathway:
+                logging.error('Cannot retreive the pathway: '+str(pathway_id))
+                return False
+            brsynth_annot = rpsbml.readBRSYNTHAnnotation(rp_pathway.getAnnotation())
+            try:
+                file_names_score[sbml_path] = brsynth_annot['global_score']['value']
+            except KeyError:
+                logging.warning('Cannot retreive the global_score from: '+str(file_name)+'('+str(brsynth_annot)+')')
+                pass
+        logging.info(file_names_score)
+        top_file_names = [k for k, v in sorted(file_names_score.items(), key=lambda item: item[1], reverse=True)][:topX]
+        logging.info(top_file_names)
+        for todel in [i for i in glob.glob(tmpInputFolder+'/*') if i not in top_file_names]:
+            os.remove(todel)
+        #### convert to SBOL ####
         rpTool.convert(tmpInputFolder, outputSBOL, rbs, max_prot_per_react, tirs, pathway_id)
     return True
--- tool_rpSBMLtoSBOL.py
+++ tool_rpSBMLtoSBOL.py
@@ -7,6 +7,7 @@ import tarfile
 import tempfile
 import os
 import glob
+import logging
 
 import sys
 sys.path.insert(0, '/home/')
@@ -17,12 +18,14 @@ import rpToolServe
 #
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('Given an rpSBML or a collection of rpSBML in a tar, convert to the SBOL format based on the selenzyme UNIPROT IDs')
-    parser.add_argument('-input', type=str)
-    parser.add_argument('-input_format', type=str)
-    parser.add_argument('-output', type=str)
-    parser.add_argument('-rbs', type=bool, default=True)
-    parser.add_argument('-max_prot_per_react', type=int, default=3)
-    parser.add_argument('-pathway_id', type=str, default='rp_pathway')
+    parser.add_argument('-input', type=str, help='input file path', required=True)
+    parser.add_argument('-input_format', type=str, help='input file format. May be either "tar" (i.e. collection of SBML in tar archive) or "sbml"', required=True)
+    parser.add_argument('-output', type=str, help='output SBOL file path', required=True)
+    parser.add_argument('-rbs', type=str, default='True', help='calculate the RBS?')
+    parser.add_argument('-topX', type=int, default=5, help='convert the top X scoring SBML files')
+    parser.add_argument('-tirs', type=str, default='10000,20000,30000', help='RBS numbers relative strengths (must be the same length of "max_prot_per_react")')
+    parser.add_argument('-max_prot_per_react', type=int, default=3, help='number of RBS per reactions')
+    parser.add_argument('-pathway_id', type=str, default='rp_pathway', help='name of the heterologous pathway in the SBML files')
     params = parser.parse_args()
     if params.rbs=='True' or params.rbs=='true' or params.rbs==True or params.rbs=='T':
         rbs = True
@@ -30,20 +33,26 @@ if __name__ == "__main__":
         rbs = False
     else:
         logging.error('Cannot parse the rbs input: '+str(params.rbs))
-    #TODO: for the moment we will consider this parameter to be always None, need to change that
-    tirs = None
+        exit(1)
+    try:
+        tirs = [int(i) for i in params.tirs.split(',')]
+    except ValueError:
+        logging.error('Invalid tirs input: '+str(params.tirs))
+        exit(1)
+    #TODO: add check that the -tirs length is the same as -max_prot_per_react
     if params.input_format=='tar':
         rpToolServe.runRPSBMLtoSBOL_hdd(params.input,
                                         params.output,
                                         rbs,
                                         params.max_prot_per_react,
                                         tirs,
+                                        params.topX,
                                         params.pathway_id)
     elif params.input_format=='sbml':
         #make the tar.xz 
         with tempfile.TemporaryDirectory as tmpOutputFolder:
-            input_tar = tmpOutputFolder+'/tmp_input.tar.xz'
-            with tarfile.open(input_tar, mode='w:xz') as tf:
+            input_tar = tmpOutputFolder+'/tmp_input.tar'
+            with tarfile.open(input_tar, mode='w:gz') as tf:
                 info = tarfile.tarinfo('single.rpsbml.xml') #need to change the name since galaxy creates .dat files
                 info.size = os.path.getsize(params.input)
                 tf.addfile(tarinfo=info, fileobj=open(params.input, 'rb')) 
@@ -52,6 +61,8 @@ if __name__ == "__main__":
                                             rbs,
                                             params.max_prot_per_react,
                                             tirs,
+                                            params.topX,
                                             params.pathway_id)
     else:
         self.logging('cannot identify the input_format: '+str(params.input_format))
+        exit(1)
